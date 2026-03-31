7112922564f9e36b2b255b1cf112f67019d3d9af
adam-ghavi <adamghaviyasha@gmail.com>
2026-03-31 20:35:09 +0700

    Implement receive_notification function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -18,6 +18,13 @@ impl NotificationService {
     }
 }
 
+pub fn subscribe(product_type: &str) -> Result<SubscriberRequest> {
+    let product_type_clone = String::from(product_type);
+    return thread::spawn(move || Self::subscribe_request(product_type_clone))
+        .join()
+        .unwrap();
+}
+
 pub fn unsubscribe(product_type: &str, url: &str) -> Result<Subscriber> {
     let product_type_upper: String = product_type.to_uppercase();
     let product_type_str: &str = product_type_upper.as_str();
@@ -29,4 +36,9 @@ pub fn unsubscribe(product_type: &str, url: &str) -> Result<Subscriber> {
         ));
     }
     return Ok(result.unwrap());
+}
+
+pub fn receive_notification(payload: Notification) -> Result<Notification> {
+    let subscriber_result: Notification = NotificationRepository::add(payload);
+    return Ok(subscriber_result);
 }
